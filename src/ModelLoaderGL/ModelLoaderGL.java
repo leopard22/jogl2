12fb45a57070dd0f2384d55d1c4a9efa88456caf
COSSON Mathieu <cosson.mathieu2@gmail.com>
2014-02-12 15:55:57 +0100

    chargement + camera
    chargement du fichier amélioré + modification de la vue grâce aux
    boutons

--- src/ModelLoaderGL/ModelLoaderGL.java
+++ src/ModelLoaderGL/ModelLoaderGL.java
@@ -42,7 +42,7 @@ import javax.media.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.*;    // for FPSAnimator
 
 
-public class ModelLoaderGL extends JFrame
+public class ModelLoaderGL //extends JFrame
 {
   private static int DEFAULT_FPS = 80;
 
@@ -50,29 +50,32 @@ public class ModelLoaderGL extends JFrame
   private static final int PHEIGHT = 512; 
 
   private static final float MAX_SIZE = 4.0f;  // for a model's dimension
-
+  private JFrame frame;
   private ModelLoaderGLListener listener;
   private FPSAnimator animator;
   private JPanel p;
   private Container c;
-  private boolean doRotate;
+  private boolean doRotate = true;
+  private JPanel panel_1;
 
-
+  /**
+   * @wbp.parser.entryPoint
+   */
   public ModelLoaderGL(String nm, float maxSize, boolean doRotate) 
   {
-    super("ModelLoaderGL");
+    //super("ModelLoaderGL");
+	  frame = new JFrame("ModelLoaderJOGL");
+	  frame.setResizable(false);
+	  frame.setSize(800, 600);
 
     System.out.println("Max model size: " + maxSize);
     System.out.println("Rotating: " + doRotate);
 
-    c = getContentPane();
-    c.setLayout( new BorderLayout() );
-	p = makeRenderPanel(nm, MAX_SIZE, true);
-    c.add(p, BorderLayout.CENTER);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    c = frame.getContentPane();
+    //ouvrirOBJ();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
 	JPanel panel = new JPanel();
-	c.add(panel, BorderLayout.EAST);
 	
 	JButton button = new JButton("Sauvegarder...");
 	button.addActionListener(new ActionListener() {
@@ -85,29 +88,14 @@ public class ModelLoaderGL extends JFrame
 	btnNewButton.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("fermer.");
+			
 		}
 	});
-	
-	JButton btnNewButton_1 = new JButton("Palette couleur\n");
+	JButton btnNewButton_1 = new JButton("Ouvrir...\n");
 	btnNewButton_1.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-
-			
-			//Create and set up the window.
-	        JFrame frame = new JFrame("ColorChooser");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	        //Create and set up the content pane.
-	        JComponent newContentPane = new ColorChooserDemo();
-	        newContentPane.setOpaque(true); //content panes must be opaque
-	        frame.setContentPane(newContentPane);
-
-	        //Display the window.
-	        frame.pack();
-	        frame.setVisible(true);
-			
-
-		
+			System.out.println("rotation");
+			ouvrirOBJ();
 		}
 	});
 	
@@ -122,6 +110,7 @@ public class ModelLoaderGL extends JFrame
 	button_1.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
 			System.out.println("gauche");
+			listener.rotateLeft();
 		}
 	});
 	
@@ -129,6 +118,7 @@ public class ModelLoaderGL extends JFrame
 	button_2.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
 			System.out.println("haut");
+			listener.rotateUp();
 		}
 	});
 	
@@ -136,6 +126,8 @@ public class ModelLoaderGL extends JFrame
 	button_3.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("droite");
+			listener.rotateRight();
+
 		}
 	});
 	
@@ -143,6 +135,7 @@ public class ModelLoaderGL extends JFrame
 	button_4.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("bas");
+			listener.rotateDown();
 		}
 	});
 	
@@ -235,8 +228,29 @@ public class ModelLoaderGL extends JFrame
 	);
 	panel.setLayout(gl_panel);
 	
+	panel_1 = new JPanel();
+	GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+	groupLayout.setHorizontalGroup(
+		groupLayout.createParallelGroup(Alignment.LEADING)
+			.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+				.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 508, Short.MAX_VALUE)
+				.addGap(18)
+				.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+	);
+	groupLayout.setVerticalGroup(
+		groupLayout.createParallelGroup(Alignment.LEADING)
+			.addGroup(groupLayout.createSequentialGroup()
+				.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+					.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+						.addContainerGap()
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 417, GroupLayout.PREFERRED_SIZE)))
+				.addContainerGap(133, Short.MAX_VALUE))
+	);
+	frame.getContentPane().setLayout(groupLayout);
+	
 	JMenuBar menuBar = new JMenuBar();
-	this.setJMenuBar(menuBar);
+	frame.setJMenuBar(menuBar);
 	
 	JMenu mnFichier = new JMenu("Fichier");
 	menuBar.add(mnFichier);
@@ -244,39 +258,7 @@ public class ModelLoaderGL extends JFrame
 	JMenuItem mntmOuvrir = new JMenuItem("Ouvrir...");
 	mntmOuvrir.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
-			String path="";
-			  FileFilter objFile = new FileFilter() {
-					
-					@Override
-					public String getDescription() {
-						return "Modele OBJ";
-					}
-					
-					@Override
-					public boolean accept(File f) {
-						if (f.getName().toLowerCase().endsWith("obj") || f.isDirectory()) {
-							return true;
-						}
-						return false;
-					}
-				};
-				JFileChooser choixFichier = new JFileChooser();
-				choixFichier.addChoosableFileFilter(objFile);
-				choixFichier.setFileFilter(objFile);
-				int retour = choixFichier.showOpenDialog(null);
-				if (retour == JFileChooser.APPROVE_OPTION) {
-					File choix = choixFichier.getSelectedFile();
-					path = choix.getAbsolutePath();
-					if (path.endsWith(".obj")) {
-						// ajout du modelloader
-						c.remove(p);
-						p = makeRenderPanel(path, MAX_SIZE, true);
-						c.add(p , BorderLayout.CENTER);
-						}
-					else {
-						JOptionPane.showMessageDialog(null, "Type de fichier non pris en charge", "Erreur", 0);
-					}
-				}
+			ouvrirOBJ();
 		}
 	});
 	mnFichier.add(mntmOuvrir);
@@ -286,25 +268,9 @@ public class ModelLoaderGL extends JFrame
 	
 	JMenuItem mntmSauvergarder = new JMenuItem("Sauvergarder...");
 	mnFichier.add(mntmSauvergarder);
+    frame.setVisible(true);
+
     
-    addWindowListener( new WindowAdapter() {
-      public void windowClosing(WindowEvent e)
-      /* The animator must be stopped in a different thread from
-         the AWT event queue, to make sure that it completes before
-         exit is called. */
-      { new Thread( new Runnable() {
-          public void run() {
-            animator.stop();
-            System.exit(0);
-          }
-        }).start();
-      } // end of windowClosing()
-    });
-
-    pack();
-    setVisible(true);
-
-    animator.start();
   } // end of ModelLoaderGL()
 
 
@@ -318,7 +284,9 @@ public class ModelLoaderGL extends JFrame
     renderPane.setPreferredSize( new Dimension(PWIDTH, PHEIGHT));
 
     GLCanvas canvas = new GLCanvas(); 
-
+    //FPSMouseControler = new FPSMouseController();
+    //fpsMouseController.setCanvas(mycanvas);
+    //listener = null;
     listener = new ModelLoaderGLListener(nm, maxSize, doRotate); 
     canvas.addGLEventListener(listener);
 
@@ -331,6 +299,80 @@ public class ModelLoaderGL extends JFrame
 
 // -----------------------------------------
 
+  public void ouvrirOBJ(){
+
+		String path="";
+		  FileFilter objFile = new FileFilter() {
+				
+				@Override
+				public String getDescription() {
+					return "Modele OBJ";
+				}
+				
+				@Override
+				public boolean accept(File f) {
+					if (f.getName().toLowerCase().endsWith("obj") || f.isDirectory()) {
+						return true;
+					}
+					return false;
+				}
+			};
+			JFileChooser choixFichier = new JFileChooser();
+			choixFichier.addChoosableFileFilter(objFile);
+			choixFichier.setFileFilter(objFile);
+			int retour = choixFichier.showOpenDialog(null);
+			if (retour == JFileChooser.APPROVE_OPTION) {
+				File choix = choixFichier.getSelectedFile();
+				path = choix.getAbsolutePath();
+				if (path.endsWith(".obj")) {
+					// ajout du modelloader
+					if(p!= null){panel_1.remove(p);p=null;}
+					p = makeRenderPanel(path, MAX_SIZE, true);
+					//p.revalidate();
+					panel_1.add(p);
+					//c.add(p , BorderLayout.CENTER);
+					
+					/*
+					 * ajouter Keylistner & mouselistner
+					 * 
+					 */
+					
+					frame.addWindowListener( new WindowAdapter() {
+					      public void windowClosing(WindowEvent e)
+					      /* The animator must be stopped in a different thread from
+					         the AWT event queue, to make sure that it completes before
+					         exit is called. */
+					      { new Thread( new Runnable() {
+					          public void run() {
+					            animator.stop();
+					            System.exit(0);
+					          }
+					        }).start();
+					      } // end of windowClosing()
+					    });
+
+					    //frame.pack();
+
+					    animator.start();
+					
+					
+					
+					
+					p.revalidate();
+					
+					
+					
+					
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Type de fichier non pris en charge", "Erreur", 0);
+				}
+			}
+	
+  }
+  /**
+   * @wbp.parser.entryPoint
+   */
   public static void main(String[] args)
   {
     if ((args.length < 1) || (args.length > 3))
